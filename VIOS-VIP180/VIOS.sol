bd93ce3abb35ef3101bcb522bca08c7950bed76c
Oliver Chalk <oliverchalk245@gmail.com>
2019-04-21 13:05:41 +1000

    Fix open-zeppelin imports

--- VIOS-VIP180/VIOS.sol
+++ VIOS-VIP180/VIOS.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.2;
 
-import "../token/ERC20/ERC20.sol";
-import "../token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 /**
  * @title SimpleToken
